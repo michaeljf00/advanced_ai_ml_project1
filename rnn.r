e6bc1a0a70b32de8d9c4be46a2bf95aaf1901c59
Michael Joshua Fababeir <michaeljoshua@union-wl-296.dynamic2.rpi.edu>
2024-03-14 17:59:31 -0400

    fixed top 1000 and bottom 1000

--- rnn.r
+++ rnn.r
@@ -2,13 +2,10 @@
 library(data.table)
 
 # Read the data
-data <- read.csv("factors_and_stock_returns.csv")  # Replace "your_data.csv" with the path to your data file
+data <- read.csv("factors_and_stock_returns.csv")  
 
 # Convert 'Date' column to Date format
 data$Date <- as.Date(data$Date)
-
-# Calculate market equity
-data$market_equity <- data$mvel1 * data$sp
 
 # Group data by month
 grouped_data <- split(data, format(data$Date, "%Y-%m"))
@@ -22,7 +19,7 @@ for (month in names(grouped_data)) {
   group <- grouped_data[[month]]
   
   # Sort the data by market equity within each month's group
-  sorted_group <- group[order(-group$market_equity), ]
+  sorted_group <- group[order(-group$mvel1), ]
   
   # Get top 1000 stocks for the current month
   top_1000_stocks <- head(sorted_group, 1000)
@@ -44,21 +41,3 @@ print("Top 10 Stocks Each Month:")
 print(head(top_1000_stocks_per_month_df, 10))
 print("\nBottom 10 Stocks Each Month:")
 print(head(bottom_1000_stocks_per_month_df, 10))
-
-SimpleRNN <- keras_model_sequential()
-SimpleRNN %>%
-  layer_simple_rnn(units=20, 
-                   activation='tanh', 
-                   kernel_initializer="glorot_uniform", 
-                   bias_initializer="glorot_uniform", 
-                   recurrent_initializer="orthogonal", 
-                   kernel_regularizer="l1", 
-                   input_shape=c(ncol(x_train_reg),1), 
-                   unroll=T, 
-                   stateful=F) %>%
-  layer_dense(units=1, 
-              kernel_initializer="glorot_uniform", 
-              bias_initializer="glorot_uniform", 
-              kernel_regularizer="l1") %>%
-  compile(loss = 'mean_squared_error', optimizer = 'adam')
-SimpleRNN
