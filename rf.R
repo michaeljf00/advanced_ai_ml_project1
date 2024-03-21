8a05195b6f4bf34e695c1635e42bb195f10eb989
Michael Joshua Fababeir <michaeljoshua@Michaels-MacBook-Pro-6.local>
2024-03-20 20:38:10 -0400

    rf.R

--- rf.R
+++ rf.R
@@ -1,157 +1,74 @@
 rm(list=ls())
-library(dplyr)
-library(data.table)
 library(caret)
-setwd("Documents/RPI/Spring2024/Risk analytics/advanced_ai_ml_project1/")
-# Read the data
-stock_data <- read.csv("factors_and_stock_returns.csv")
+library(dplyr)
 
-# Convert 'Date' column to Date format
-stock_data$Date <- as.Date(stock_data$Date)
-
-## Sub-setting the Data Frame to be between 2009 and 2019 
-start_date = as.Date("2009-01-31")
-end_date = as.Date("2019-12-31")
-data_subset = stock_data[stock_data$Date >=start_date  & stock_data$Date <=end_date, ]
-
-sum(is.na(data_subset))
-data_subset = na.omit(data_subset)
-
-## Winsorizing the data leaving out Date and permno columns 
-numeric_cols = sapply(data_subset[, !(names(data_subset) %in% c("Date", "permno"))], is.numeric)
-
-# Loop through numeric columns and winsorize
-for (col in names(data_subset)[numeric_cols]) {
-  if (is.numeric(data_subset[[col]])) {
-    q1 = quantile(data_subset[[col]], 0.01, na.rm = TRUE)
-    q99 = quantile(data_subset[[col]], 0.99, na.rm = TRUE)
-    
-    data_subset[[col]][data_subset[[col]] < q1] = q1
-    data_subset[[col]][data_subset[[col]] > q99] = q99
-  }
-}
-
-# Get top 1000 and bottom 100 stocks
-top_1000 <- data_subset %>% 
-  arrange(desc(mvel1)) %>%
-  group_by(Date) %>%
-  slice_head(n = 1000) %>%
-  ungroup() %>%
-  select(-Date)
-
-bottom_1000 <- data_subset %>% 
-  arrange(desc(mvel1)) %>%
-  group_by(Date) %>%
-  slice_tail(n = 1000) %>%
-  ungroup() %>%
-  select(-Date)
-
+setwd("/Users/z3r0/Documents/RPI/Spring2024/ML/advanced_ai_ml_project1/")
+train_data <- read.csv("1000_unique_train.csv")
+test_data <- read.csv("1000_unique_test.csv")
 set.seed(123)
-
-## Sample Data-set with 1000 Unique permnos 
-unique_stocks = unique(data_subset$permno)
-sampled_permnos = sample(unique_stocks, size = 1000, replace = FALSE)
-sampled_1000_unique = data_subset[data_subset$permno %in% sampled_permnos, ]
-
-train_test_split <- function(sampled_data) {
-  dataset_name <- deparse(substitute(sampled_data))
-  
-  ## Output Sampled Data as a CSV
-  write.csv(sampled_data, file = paste0(dataset_name, "_stocks.csv"), row.names = FALSE)
-  
-  ## Time Based Train-Validation-Test split 
-  sampled_data$Date = as.Date(sampled_data$Date)
-  train_end_date = as.Date("2017-12-31")
-  validation_end_date = as.Date("2018-12-31")
-  
-  X <- sampled_data[, -which(names(sampled_data) %in% c("Date", "return"))]
-  y <- sampled_data$return
-  
-  train_data = subset(sampled_data, Date <= train_end_date)
-  write.csv(train_data, file = paste0(dataset_name, "_train.csv"), row.names = FALSE)
-  X_train <- as.matrix(train_data[, -which(names(train_data) %in% c("Date", "return"))])
-  X_train <- apply(X_train, 2, as.numeric)
-  y_train <- train_data$return
-  
-  validation_data = subset(sampled_data, Date > train_end_date & Date <= validation_end_date)
-  write.csv(validation_data, file = paste0(dataset_name, "_validation.csv"), row.names = FALSE)
-  X_val <- as.matrix(validation_data[, -which(names(validation_data) %in% c("Date", "return"))])
-  X_val <- apply(X_val, 2, as.numeric)
-  y_val <- validation_data$return
-  
-  test_data = subset(sampled_data, Date > validation_end_date)
-  write.csv(test_data, file = paste0(dataset_name, "_test.csv"), row.names = FALSE)
-  X_test <- as.matrix(test_data[, -which(names(test_data) %in% c("Date", "return"))])
-  X_test <- apply(X_test, 2, as.numeric)
-  y_test <- test_data$return
-  
-  
-  return(list(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test))
-}
-
-top_bottom_split <- function(dataset, train_ratio=0.7, validation_ratio=0.2, test_ratio=0.1) {
-  dataset_name <- deparse(substitute(dataset))
-  
-  # Shuffle dataset
-  shuffled_dataset <- dataset[sample(nrow(dataset)), ]
-  
-  # Compute sizes of train, validation, and test sets
-  n <- nrow(shuffled_dataset)
-  train_size <- floor(train_ratio * n)
-  validation_size <- floor(validation_ratio * n)
-  test_size <- n - train_size - validation_size
-  
-  # Split dataset
-  train_data <- shuffled_dataset[1:train_size, ]
-  write.csv(train_data, file = paste0(dataset_name, "_train.csv"), row.names = FALSE)
-  X_train <- as.matrix(train_data[, -which(names(train_data) %in% c("return"))])
-  X_train <- apply(X_train, 2, as.numeric)
-  y_train <- train_data$return
-  
-  validation_data <- shuffled_dataset[(train_size + 1):(train_size + validation_size), ]
-  write.csv(validation_data, file = paste0(dataset_name, "_validation.csv"), row.names = FALSE)
-  X_val <- as.matrix(validation_data[, -which(names(validation_data) %in% c("return"))])
-  X_val <- apply(X_val, 2, as.numeric)
-  y_val <- validation_data$return
-  
-  test_data <- shuffled_dataset[(train_size + validation_size + 1):n, ]
-  write.csv(test_data, file = paste0(dataset_name, "_test.csv"), row.names = FALSE)
-  X_test <- as.matrix(test_data[, -which(names(test_data) %in% c("return"))])
-  X_test <- apply(X_test, 2, as.numeric)
-  y_test <- test_data$return
-  
-  # Return split datasets
-  return(list(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test))
-}
-sampled_data_splits <- train_test_split(sampled_1000_unique)
-train_data <- sampled_data_splits$X_train
-train_labels <- sampled_data_splits$y_train
+sample_size <- round(0.1 * nrow(train_data))
+sampled_train_data <- train_data[sample(nrow(train_data), sample_size), ]
 
 trControl <- trainControl(method = "cv",
                           number = 5, 
                           allowParallel = TRUE,
                           verboseIter = TRUE)
-tuneGrid <- expand.grid(.mtry = 1:6)
 
-rf_model <- train(x = train_data, y = train_labels,
+rf_model <- train(return ~ .,
+                  data = sampled_train_data,
+                  method = "rf",
+                  trControl = trControl,
+                  tuneLength = 5)
+tuneGrid <- expand.grid(.mtry = 1:6)
+rf_model2 <- train(return ~ .,
+                  data = train_data,
                   method = "rf",
                   trControl = trControl,
                   tuneGrid = tuneGrid)
+train_data$date <- as.Date(train_data$Date)
+# Random Forest
+# 
+# 63267 samples
+# 23 predictor
+# 
+# No pre-processing
+# Resampling: Cross-Validated (5 fold)
+# Summary of sample sizes: 50613, 50615, 50612, 50614, 50614
+# Resampling results across tuning parameters:
+# 
+#   mtry  RMSE       Rsquared   MAE
+# 1     0.1235279  0.1459196  0.08605488
+# 2     0.1189797  0.1712982  0.08230454
+# 3     0.1167004  0.1787465  0.08034506
+# 4     0.1156640  0.1823752  0.07947514
+# 5     0.1150910  0.1852811  0.07900168
+# 6     0.1146624  0.1881083  0.07868445
+# 
+# RMSE was used to select the optimal model using the smallest value.
+# The final value used for the model was mtry = 6.
 
-print(rf_model)
+# Initialize a data frame to store complexity measures
+complexity_measures <- data.frame(Year = integer(), Complexity = double())
+# Top 10 R^2 values:
+#   > print(top_10_r2)
+# [1] 0.107558
+# > cat("\nBottom 10 R^2 values:\n")
+# 
+# Bottom 10 R^2 values:
+#   > print(bottom_10_r2)
+# [1] 0.107558
+# > cat("\nOverall R^2:", overall_r2, "\n")
+# 
+# Overall R^2: 0.107558 
+r2_values <- rf_model$results$Rsquared
 
+top_10_r2 <- head(sort(r2_values, decreasing = TRUE), 10)
+bottom_10_r2 <- tail(sort(r2_values), 10)
 
-# Prepare the test data
-X_test <- sampled_data_splits$X_test
-y_test <- sampled_data_splits$y_test
+overall_r2 <- rf_model$results$Rsquared[which.max(rf_model$results$Rsquared)]
 
-# Make predictions on the test data
-predictions <- predict(rf_model, X_test)
-rmse <- sqrt(mean((y_test - predictions)^2))
-print(paste("RMSE:", rmse))
-
-mae <- mean(abs(y_test - predictions))
-print(paste("MAE:", mae))
-R_squared <- 1 - sum((y_test - predictions)^2) / sum((y_test - mean(y_test))^2)
-print(paste("R-squared:", R_squared))
-
+cat("Top 10 R^2 values:\n")
+print(top_10_r2)
+cat("\nBottom 10 R^2 values:\n")
+print(bottom_10_r2)
+cat("\nOverall R^2:", overall_r2, "\n")
